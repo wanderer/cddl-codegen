8d73d99cc1342ff17dd0f707ebc7b70854dfffbb
Martin Becze <martin@becze.org>
2025-03-05 22:54:45 +0000

    Added a nix flake for cddl-codegen

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "cddl-codegen: Generate Rust, WASM and JSON code from CDDL specifications";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          overlays = [(import rust-overlay)];
+          inherit system;
+        };
+
+        rustToolchain = pkgs.rust-bin.nightly.latest.default;
+
+        nativeBuildInputs = with pkgs; [
+          rustToolchain
+          pkg-config
+          which
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = nativeBuildInputs;
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "cddl-codegen";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+
+          # the regular tests don't run yet
+          checkPhase = ''
+            cargo test comment_ast
+          '';
+
+          inherit nativeBuildInputs;
+
+          meta = with pkgs.lib; {
+            description = "Codegen serialization logic for CBOR automatically from a CDDL specification";
+            homepage = "https://github.com/dcSpark/cddl-codegen";
+            license = licenses.mit;
+            maintainers = [];
+          };
+        };
+      }
+    );
+}
